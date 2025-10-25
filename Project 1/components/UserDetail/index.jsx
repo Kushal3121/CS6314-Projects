21e0c1b9c1a8ee305cc398afa733f75487d793ad
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-10-25 00:09:08 -0500

    user details

--- Project 1/components/UserDetail/index.jsx
+++ Project 1/components/UserDetail/index.jsx
@@ -1,26 +1,47 @@
-import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import PropTypes from 'prop-types';
-import { Typography } from '@mui/material';
-
+import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Paper, Typography, Stack } from '@mui/material';
 import './styles.css';
 
-function UserDetail({ userId }) {
+export default function UserDetail() {
+  const { userId } = useParams(); // extract from URL
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    let mounted = true;
+    async function loadUser() {
+      try {
+        const { data } = await axios.get(`/user/${userId}`);
+        if (mounted) setUser(data);
+      } catch (err) {
+        console.error('Failed to load user detail:', err);
+      }
+    }
+    loadUser();
+    return () => {
+      mounted = false;
+    };
+  }, [userId]);
+
+  if (!user) return <Typography>Loading...</Typography>;
+
   return (
-    <Typography variant="body1">
-      This should be the UserDetail view of the PhotoShare app. Since it is
-      invoked from React Router the params from the route will be in the
-      property userId. So this should show details of user:
-      {' '}
-      {userId}
-      . You can
-      fetch the model for the user from API: /user/:id
-    </Typography>
+    <Paper elevation={0} style={{ padding: '16px' }}>
+      <Typography variant='h6' gutterBottom>
+        {user.first_name} {user.last_name}
+      </Typography>
+
+      <Stack spacing={0.5} sx={{ mb: 2 }}>
+        <Typography variant='body2'>Location: {user.location}</Typography>
+        <Typography variant='body2'>Occupation: {user.occupation}</Typography>
+        <Typography variant='body2'>Description: {user.description}</Typography>
+      </Stack>
+
+      {/* Link to Photos */}
+      <Link to={`/photos/${user._id}`} style={{ color: '#1976d2' }}>
+        View Photos
+      </Link>
+    </Paper>
   );
 }
-
-UserDetail.propTypes = {
-  userId: PropTypes.string.isRequired,
-};
-
-export default UserDetail;
