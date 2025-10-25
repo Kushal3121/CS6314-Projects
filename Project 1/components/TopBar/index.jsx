87045ba0337e3f666a565134bcae1d725375722c
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-10-25 00:26:47 -0500

    topbar

--- Project 1/components/TopBar/index.jsx
+++ Project 1/components/TopBar/index.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import './styles.css';
+export default function TopBar() {
+  const location = useLocation();
+  const [rightText, setRightText] = useState('');
 
-function TopBar() {
+  useEffect(() => {
+    async function update() {
+      const path = location.pathname;
+
+      // /users/<id>
+      if (path.startsWith('/users/')) {
+        const userId = path.split('/')[2];
+        const { data } = await axios.get(`/user/${userId}`);
+        setRightText(`${data.first_name} ${data.last_name}`);
+      }
+
+      // /photos/<id>
+      else if (path.startsWith('/photos/')) {
+        const userId = path.split('/')[2];
+        const { data } = await axios.get(`/user/${userId}`);
+        setRightText(`Photos of ${data.first_name} ${data.last_name}`);
+      }
+
+      // /users
+      else if (path === '/users') {
+        setRightText('User List');
+      } else {
+        setRightText('');
+      }
+    }
+
+    update();
+  }, [location]);
+
   return (
-    <AppBar className="topbar-appBar" position="absolute">
+    <AppBar position='static'>
       <Toolbar>
-        <Typography variant="h5" color="inherit">
-          This is the TopBar component
+        <Typography variant='h6' sx={{ flexGrow: 1 }}>
+          Kushal Choudhary
         </Typography>
+        <Typography variant='h6'>{rightText}</Typography>
       </Toolbar>
     </AppBar>
   );
 }
-
-export default TopBar;
