1c142e9c543edc23e2731a9f78b1580ddc779c7d
Kushals-Air <kushalchoudhary3121@gmail.com>
2025-10-25 15:42:27 -0500

    top bar styling

--- Project 1/components/TopBar/index.jsx
+++ Project 1/components/TopBar/index.jsx
@@ -2,40 +2,68 @@ import React, { useEffect, useState } from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import './styles.css';
 
+/**
+ * TopBar:
+ * - Shows developer name (left)
+ * - Shows context for current view (right)
+ *   -> User Name OR "Photos of ..." OR "User List"
+ */
 export default function TopBar() {
   const location = useLocation();
   const [rightText, setRightText] = useState('');
 
   useEffect(() => {
-    async function update() {
+    /**
+     * Derive the correct right-hand text based on current route.
+     * Uses axios to fetch the user name when needed.
+     */
+    async function updateRightText() {
       const path = location.pathname;
 
+      // When viewing a specific User Detail
       if (path.startsWith('/users/')) {
         const userId = path.split('/')[2];
         const { data } = await axios.get(`/user/${userId}`);
         setRightText(`${data.first_name} ${data.last_name}`);
-      } else if (path.startsWith('/photos/')) {
+      }
+
+      // When viewing a specific User's Photos
+      else if (path.startsWith('/photos/')) {
         const userId = path.split('/')[2];
         const { data } = await axios.get(`/user/${userId}`);
         setRightText(`Photos of ${data.first_name} ${data.last_name}`);
-      } else if (path === '/users') {
+      }
+
+      // When viewing user list
+      else if (path === '/users') {
         setRightText('User List');
-      } else {
+      }
+
+      // Default fallback for home or unknown route
+      else {
         setRightText('');
       }
     }
 
-    update();
+    updateRightText();
   }, [location]);
 
   return (
-    <AppBar position='static'>
-      <Toolbar>
-        <Typography variant='h6' sx={{ flexGrow: 1 }}>
+    <AppBar
+      position='static'
+      elevation={3}
+      color='primary'
+      className='topbar-appBar'
+    >
+      <Toolbar className='topbar-toolbar'>
+        <Typography variant='h6' className='topbar-left'>
           Kushal Choudhary
         </Typography>
-        <Typography variant='h6'>{rightText}</Typography>
+        <Typography variant='h6' className='topbar-right'>
+          {rightText}
+        </Typography>
       </Toolbar>
     </AppBar>
   );
